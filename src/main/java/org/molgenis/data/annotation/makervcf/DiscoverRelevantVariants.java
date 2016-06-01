1015696bcbdb4b27a11cc41805bf219514ec61d1
K. Joeri van der Velde <joeriv@gmail.com>
2016-06-01 12:38:20 +0200

    Setting up project structure and doc

--- src/main/java/org/molgenis/data/annotation/makervcf/DiscoverRelevantVariants.java
+++ src/main/java/org/molgenis/data/annotation/makervcf/DiscoverRelevantVariants.java
@@ -1,5 +1,12 @@
 package org.molgenis.data.annotation.makervcf;
 
+import org.molgenis.data.Entity;
+import org.molgenis.data.vcf.VcfRepository;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
 /**
  * Created by joeri on 6/1/16.
  *
@@ -9,4 +16,19 @@ package org.molgenis.data.annotation.makervcf;
  *
  */
 public class DiscoverRelevantVariants {
+
+    public DiscoverRelevantVariants(File vcfFile) throws IOException {
+
+        VcfRepository vcf = new VcfRepository(vcfFile, "vcf");
+        Iterator<Entity> it = vcf.iterator();
+
+
+        while (it.hasNext())
+        {
+
+        }
+
+        
+    }
+
 }
---
+++ src/main/java/org/molgenis/data/annotation/makervcf/clinvar/ClinVar.java
@@ -0,0 +1,11 @@
+package org.molgenis.data.annotation.makervcf.clinvar;
+
+/**
+ * Created by joeri on 6/1/16.
+ */
+public class ClinVar {
+
+    //  /Users/joeri/github/clinvar.5.5.16/clinvar.patho.fix.5.5.16.vcf.gz
+
+
+}
