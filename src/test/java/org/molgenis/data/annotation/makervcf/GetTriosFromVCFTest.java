5a3326c5eae158a85e8884da2e90973babff98a5
K. Joeri van der Velde <joeriv@gmail.com>
2016-09-28 12:47:47 +0200

    Full test of loading pedigree data from vcf

--- src/test/java/org/molgenis/data/annotation/makervcf/GetTriosFromVCFTest.java
+++ src/test/java/org/molgenis/data/annotation/makervcf/GetTriosFromVCFTest.java
@@ -14,10 +14,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -43,16 +40,35 @@ public class GetTriosFromVCFTest
 		HashMap<String, Trio> trios = td.getTrios();
 		Set<String> parents = td.getParents();
 
-		System.out.println(trios.toString());
-		System.out.println(parents.toString());
-
-
 		assertEquals(trios.size(), 6);
 		assertEquals(parents.size(), 7);
 
-		
+		assertEquals(trios.get("p01").getChild().getId(), "p01");
+		assertEquals(trios.get("p01").getMother().getId(), "p02");
+		assertEquals(trios.get("p01").getFather().getId(), "p03");
 
+		assertEquals(trios.get("p04").getChild().getId(), "p04");
+		assertEquals(trios.get("p04").getMother().getId(), "p02");
+		assertEquals(trios.get("p04").getFather().getId(), "p03");
 
+		assertEquals(trios.get("p05").getChild().getId(), "p05");
+		assertEquals(trios.get("p05").getMother().getId(), "p02");
+		assertEquals(trios.get("p05").getFather().getId(), "p06");
+
+		assertEquals(trios.get("p07").getChild().getId(), "p07");
+		assertEquals(trios.get("p07").getMother().getId(), "p08");
+		assertEquals(trios.get("p07").getFather().getId(), "p09");
+
+		assertEquals(trios.get("p10").getChild().getId(), "p10");
+		assertEquals(trios.get("p10").getMother().getId(), "p11");
+		assertEquals(trios.get("p10").getFather(), null);
+
+		assertEquals(trios.get("p12").getChild().getId(), "p12");
+		assertEquals(trios.get("p12").getMother(), null);
+		assertEquals(trios.get("p12").getFather().getId(), "p13");
+
+		HashSet expectedParents = new HashSet<String>(Arrays.asList("p02", "p03", "p06", "p08", "p09", "p11", "p13"));
+		assertEquals(parents, expectedParents);
 	}
 
 }
