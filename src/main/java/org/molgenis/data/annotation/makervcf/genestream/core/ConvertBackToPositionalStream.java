7b391e63044ae5e93fb691d291b121030bfc8a43
K. Joeri van der Velde <joeriv@gmail.com>
2016-10-07 13:13:07 +0200

    Switching back to positional stream now supports duplicate site positions

--- src/main/java/org/molgenis/data/annotation/makervcf/genestream/core/ConvertBackToPositionalStream.java
+++ src/main/java/org/molgenis/data/annotation/makervcf/genestream/core/ConvertBackToPositionalStream.java
@@ -2,10 +2,7 @@ package org.molgenis.data.annotation.makervcf.genestream.core;
 
 import org.molgenis.data.annotation.makervcf.structs.RelevantVariant;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * Created by joeri on 6/29/16.
@@ -27,7 +24,7 @@ public class ConvertBackToPositionalStream {
     public Iterator<RelevantVariant> go() {
         return new Iterator<RelevantVariant>() {
 
-            TreeMap<Integer, RelevantVariant> buffer = new TreeMap<Integer, RelevantVariant>();
+            TreeMap<Integer, ArrayList<RelevantVariant>> buffer = new TreeMap<>();
             Iterator<RelevantVariant> bufferPrinter;
             RelevantVariant nextResult;
             int i = -1;
@@ -62,7 +59,10 @@ public class ConvertBackToPositionalStream {
                             }
                             else
                             {
-                                buffer.put(pos, rv);
+                                // add to list of potential duplicate site positions
+                                if(buffer.containsKey(pos)) { buffer.get(pos).add(rv); }
+                                else { buffer.put(pos, new ArrayList(Arrays.asList(rv))); }
+
                                 if(verbose){ System.out.println("[ConvertBackToPositionalStream] Buffer size > 0, adding to buffer " + pos); }
 
                                 // check if all positions are present up to the current one
@@ -76,7 +76,7 @@ public class ConvertBackToPositionalStream {
                                     }
                                 }
 
-                                bufferPrinter = buffer.values().iterator();
+                                bufferPrinter = getIterator(buffer);
                                 if(verbose){ System.out.println("[ConvertBackToPositionalStream] Positions aligned again at "+pos+", all values smaller than current pos, so clearning buffer with "+buffer.size()+" elements");}
                                 buffer = new TreeMap<>();
                                 nextResult = bufferPrinter.next();
@@ -86,9 +86,9 @@ public class ConvertBackToPositionalStream {
                         }
                         else
                         {
-                            //buffer
                             if(verbose){ System.out.println("[ConvertBackToPositionalStream] Adding to buffer " + pos); }
-                            buffer.put(pos, rv);
+                            if(buffer.containsKey(pos)) { buffer.get(pos).add(rv); }
+                            else { buffer.put(pos, new ArrayList(Arrays.asList(rv))); }
                         }
                         
                     } catch (Exception e) {
@@ -96,9 +96,11 @@ public class ConvertBackToPositionalStream {
                     }
                 }
 
+                bufferPrinter = getIterator(buffer);
                 if(bufferPrinter != null && bufferPrinter.hasNext())
                 {
                     if(verbose){ System.out.println("[ConvertBackToPositionalStream] Clearing last elements from buffer");}
+                    buffer = new TreeMap<>();
                     nextResult = bufferPrinter.next();
                     return true;
                 }
@@ -111,5 +113,16 @@ public class ConvertBackToPositionalStream {
                 return nextResult;
             }
         };
+    }
+
+    private Iterator<RelevantVariant> getIterator(TreeMap<Integer, ArrayList<RelevantVariant>> buffer)
+    {
+        List<RelevantVariant> variants = new ArrayList<>();
+        //keys supposed to be sorted because TreeMap
+        for(Integer pos : buffer.keySet())
+        {
+            variants.addAll(buffer.get(pos));
+        }
+        return variants.iterator();
     }
 }
