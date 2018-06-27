c69b8b6bda3920246670e9ea57d4aaba2cd564ab
K. Joeri van der Velde <joeriv@gmail.com>
2018-06-27 15:16:52 +0200

    Update for plotting FDR and FOR results

--- src/main/R/FDR_plot.R
+++ src/main/R/FDR_plot.R
@@ -38,3 +38,43 @@ ggplot() +
   scale_y_log10(breaks = breaks)
 
 #save as 12 x 7 inch for good scaling
+
+##
+# UPDATE june 2018
+##
+
+# load FDR and CGD sets, newer versions
+fdr1.0 <- read.table("bundle_r1.0/FDR_allGenes_r1.0.tsv", sep="\t", header=T)
+fdr1.2 <- read.table("bundle_r1.2/FDR_allGenes_r1.2.tsv", sep="\t", header=T)
+cgd <- read.table("GenesInheritance26jun2018.tsv", sep="\t", header=T)
+
+# old means/medians
+mean(fdr1.0$AffectedFrac)*100
+median(fdr1.0$AffectedFrac)*100
+mean(fdr1.0$CarrierFrac)*100
+median(fdr1.0$CarrierFrac)*100
+
+# new means/medians
+mean(fdr1.2$AffectedFrac)*100
+median(fdr1.2$AffectedFrac)*100
+mean(fdr1.2$CarrierFrac)*100
+median(fdr1.2$CarrierFrac)*100
+
+# merge data and calculate difference, keep CGD genes
+mfdr <- merge(fdr1.0, fdr1.2, by="Gene")
+mfdr$AffectedFracDiff = mfdr$AffectedFrac.x-mfdr$AffectedFrac.y
+mfdr$CarrierFracDiff = mfdr$CarrierFrac.x-mfdr$CarrierFrac.y
+mfdrCGD <- merge(mfdr, cgd, by = "Gene", all.x = TRUE)
+mfdrCGD <- mfdrCGD[!is.na(mfdrCGD$Inheritance),]
+
+# basic plot of difference
+plot(mfdrCGD$AffectedFracDiff~mfdrCGD$CarrierFracDiff)
+
+# fancy plot of difference
+ggplot() +
+  theme_bw() + theme(panel.grid.major = element_line(colour = "black"), axis.text=element_text(size=16),  axis.title=element_text(size=16,face="bold")) +
+  geom_point(data = mfdrCGD, aes(x = AffectedFracDiff, y = CarrierFracDiff, shape = Inheritance, colour = Inheritance), size=3, stroke = 3, alpha=0.75) +
+  geom_density_2d(data = mfdrCGD, aes(x = AffectedFracDiff, y = CarrierFracDiff), colour="Black") +
+  geom_text(data = mfdrCGD, aes(x = AffectedFracDiff, y = CarrierFracDiff, label = Gene), hjust = 0, nudge_x = 0.01, size = 3, check_overlap = TRUE) +
+  ylab("Carrier fraction difference") +
+  xlab("Affected fraction difference")
--- src/main/R/FOR_plot.R
+++ src/main/R/FOR_plot.R
@@ -18,3 +18,35 @@ ggplot() +
   xlab("Variants expected (i.e. total per gene)")
 
 ggsave("FOR_plot.pdf", width = 10, height = 6)
+
+##
+## update
+##
+
+# read old and new FDR data, show means
+falseOmRate1.0 <- read.table("FOR_results_per_gene_r1.0.tsv", sep="\t", header=T)
+mean(falseOmRate1.0$MissedFrac)
+falseOmRate1.2 <- read.table("FOR_results_per_gene_r1.2.tsv", sep="\t", header=T)
+mean(falseOmRate1.2$MissedFrac)
+
+# calculate missed and plot for inspection
+falseOmRate1.2$missed <- falseOmRate1.2$Expected-falseOmRate1.2$Observed
+falseOmRate1.0$missed <- falseOmRate1.0$Expected-falseOmRate1.0$Observed
+plot(falseOmRate1.0$missed ~ falseOmRate1.2$missed)
+
+# merge, keeping only CGD genes
+mfor <- merge(falseOmRate1.0, falseOmRate1.2, by="Gene")
+cgd <- read.table("GenesInheritance26jun2018.tsv", sep="\t", header=T)
+mforCGD <- merge(mfor, cgd, by = "Gene", all.x = TRUE)
+mforCGD <- mforCGD[!is.na(mforCGD$Inheritance),]
+
+# go go gadget ggplot to show differences nicely
+ggplot() +
+  theme_bw() + theme(panel.grid = element_blank()) +
+  geom_abline(colour="gray", size=5) +
+  geom_point(data = mforCGD, aes(x = missed.x, y = missed.y, shape = Inheritance, colour = Inheritance), size=3, stroke = 3, alpha=0.75) +
+  geom_text(data = mforCGD, aes(x = missed.x, y = missed.y, label = Gene), hjust = 0, nudge_x = 0.01, size = 3, check_overlap = TRUE) +
+  ylab("Variants missed in r1.2") +
+  xlab("Variants missed in r1.0")
+
+ggsave("FOR_comp_plot.pdf", width = 10, height = 6)
